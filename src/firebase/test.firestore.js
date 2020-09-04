3375aa5a366bdba0a51e415a9eec91e38ce4f434
Radu <cazacuraduic@gmail.com>
2020-09-04 14:06:57 +0300

    Implemented sign-up with firebase

---
+++ src/firebase/test.firestore.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('tCyNGyZEDHj0qzQLbQ3U').collection('cartItems').doc('s6bCflxU1xGfJF22M3Ja')
+firestore.doc()
